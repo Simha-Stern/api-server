e69ba284d49d1bd588ed87f9061aea6d024f5a51
Simha <a0533187258@gmail.com>
2023-08-15 15:58:31 +0300

    I finished step 1

--- app.js
+++ app.js
@@ -1,43 +1,68 @@
 import express from "express";
 const app = express();
+app.use(express.json());
 const port = 3000;
+
 
 const users = [
   {
     id: "1",
     email: "aaa@aa.aa",
-    password: "12345678",
+    password: "12345678"
   },
   {
     id: "2",
     email: "aaa@aa.aa",
-    password: "12345678",
+    password: "12345678"
   },
   {
     id: "3",
     email: "aaa@aa.aa",
-    password: "12345678",
+    password: "12345678"
   },
 ];
 
-
-// app.get("/", (req, res) => {
-//     res.send("working");
-//   });
+app.get("/", (req, res) => {
+    res.send("working");
+  });
 
 app.get("/users", (req, res) => {
   res.send({users});
 });
 
-app.listen(port, () => {
-    console.log(`Server is up and running on port:${port}`);
-});
-
 app.get("/users/:id", (req, res) => {
-    const id = req.params.id;
-    res.send(users.find((user)=>user.id = id));
+  const id = req.params.id;
+  const user = users.find((user) => user.id === id);
+  if (user !== -1){
+      res.send(user);
+  }else{
+    res.status(404).send('not definde')
+  }
 });
 
-app.post('/', (req,res) => {
-    res.send()
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  users.push(userToAdd);
+  res.send("user added well!");
+});
+
+app.put("/users/:id", (req, res) => {
+    const userToAdd = req.body;
+    const id = req.params.id;
+    const oldUser = users.find((user) => (user.id === id))
+    oldUser.email = userToAdd.email;
+    oldUser.password = userToAdd.password;
+    res.send("user update well!");
 })
+
+app.delete("/users/:id", (req, res) => {
+    const id = req.params.id;
+    const deletUser = users.findIndex((user) => user.id === id);
+    users.splice(deletUser, 1);
+    res.send("user delet well!");
+
+})
+
+app.listen(port, () => {
+  console.log(`Server is up and running on port:${port}`);
+});
